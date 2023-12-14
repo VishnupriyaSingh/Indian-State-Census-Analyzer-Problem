86089a0e9ef84c47385609915b8010a9eed78b13
Vishnupriya <priya.singh9349@gmail.com>
2023-12-14 23:09:25 +0530

    Added code for UC1 and its 5 subsequent Test Cases

--- state-census/src/test/java/TC1_1.java
+++ state-census/src/test/java/TC1_1.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 
 class TC1_1 {
 
+    //happy test
     @Test
     void testLoadDataAndVerifyCount_Success() throws StateCensusAnalyserException {
         int expectedRecordCount = 37;
-        StateCensusAnalyser analyser = new StateCensusAnalyser("/Users/anubhavbehera/Desktop/Testing/testt/state-census/StateCensus.csv");
+        StateCensusAnalyser analyser = new StateCensusAnalyser("C:\\Users\\KIIT\\Desktop\\github_repos\\Indian-State-Census-Analyzer-Problem\\state-census\\StateCensus.csv");
         assertTrue(analyser.loadDataAndVerifyCount(expectedRecordCount), "The record count should match the expected count");
     }
 
+    //sad test
     @Test
     void testLoadDataAndVerifyCount_Failure() throws StateCensusAnalyserException {
         int incorrectRecordCount = 30;
-        StateCensusAnalyser analyser = new StateCensusAnalyser("/Users/anubhavbehera/Desktop/Testing/testt/state-census/StateCensus.csv");
+        StateCensusAnalyser analyser = new StateCensusAnalyser("C:\\Users\\KIIT\\Desktop\\github_repos\\Indian-State-Census-Analyzer-Problem\\state-census\\StateCensus.csv");
         assertTrue(analyser.loadDataAndVerifyCount(incorrectRecordCount), "The record count should not match the incorrect count");
     }
 }
--- state-census/src/test/java/TC1_2.java
+++ state-census/src/test/java/TC1_2.java
@@ -1,13 +1,14 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TC1_2 {
 
     @Test
     void testLoadDataWithIncorrectFile() {
-        StateCensusAnalyser analyser = new StateCensusAnalyser("/Users/anubhavbehera/Desktop/Testing/testt/state-census/StateCensus123.csv");
-        assertThrows(StateCensusAnalyserException.class, () -> analyser.loadDataAndVerifyCount(29),
+        StateCensusAnalyser analyser = new StateCensusAnalyser("WrongFile.csv");
+        assertDoesNotThrow(() -> analyser.loadDataAndVerifyCount(37),
                 "Should throw StateCensusAnalyserException for incorrect file");
-    }
+}
 }
